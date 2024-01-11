c26dbcb34e672107ff38f6c610b20f639f5d1b91
Deepak <deepakanjanigupta432@gmail.com>
2024-01-11 22:30:41 +0530

    Initial Commit Part1

---
+++ app.js
@@ -0,0 +1,37 @@
+const Heading = React.createElement("h1",{id: "heading"},"Hello from React");
+const root=ReactDOM.createRoot(document.getElementById('root'));
+root.render(Heading);
+
+console.log(Heading);
+
+
+/**
+ * <div id="parent">
+ *  <div id="child">
+ *      <h1>
+ *      </h1>
+ *  </div>
+ * </div>
+ * 
+ * createElement creates an object JS, which is used to 
+ * be converted by render method into HTML
+ * 
+ */
+//code in react for above  
+//can make array for upper layer 
+const parent= React.createElement(
+    "div",
+    {id :"parent"},
+    React.createElement(
+    "div",
+    {id:"child"},
+    [React.createElement('h1',{key:"a"},"Deepak"),
+    React.createElement('h2',{key:"b"},"Deepak")
+    ])
+);
+console.log(parent);
+root.render(parent);
+
+
+//To overcome code length in this approach
+//we need something like JSX
